4b98648c0647a50880c97ed1c2639a63ee355abd
Jithin Lal K <jithinlalk25@gmail.com>
2024-08-17 21:54:35 +0530

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Flex, Heading, Text } from "@chakra-ui/react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Flex
+      flexDirection="column"
+      alignItems="center"
+      justifyContent="center"
+      height="100vh"
+    >
+      <Heading as="h1" size="4xl">
+        Callee
+      </Heading>
+      <Text fontSize="md">Money collection made easy</Text>
+    </Flex>
   );
 }
 
